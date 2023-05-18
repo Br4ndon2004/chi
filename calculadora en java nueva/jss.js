c3fd1fd8ecab96689a4d416c47d7a33855b10ad4
Br4ndon2004 <114429903+Br4ndon2004@users.noreply.github.com>
2023-05-18 17:34:59 -0600

    Add files via upload

---
+++ calculadora en java nueva/jss.js
@@ -0,0 +1,8 @@
+function agregarnumero(numero){
+    var resultado = document.getElementById("resultado");
+    resultado.value += numero;
+}
+function agregaroperador(operador){
+    var resultado = document.getElementById("resultado");
+    resultado.value += resultado;
+}
